ab585484403be57614bfa013db07887ad205a8a1
Anna Roberts <annaroberts960608@gmail.com>
2016-04-14 12:59:24 -0400

    add background photo and logo

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php get_header(); ?>         
                 
 			<section class="row">
-                <div class="twelve columns">              
-                	<?php
-                    if ( have_posts() ) {
-                        while ( have_posts() ) {
-                        	the_post(); ?>
-                        	<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        	<?php if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('thumbnail');
-                            } ?>
-                            <?php the_excerpt();?>
-                        	<a href="<?php the_permalink(); ?>">Read More...</a>
-                        <?php } // end while                        		 
-                    } //end if
-                    ?>
+                <div class="twelve columns">
+                    <div class="logo"> 
+                        <img src="salt-logo.png">
+                    </div>               	
                 </div>
             </section>
                     

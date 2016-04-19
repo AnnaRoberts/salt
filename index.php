198509026d559b1f452ce25cb663284f29cea1cc
Anna Roberts <annaroberts960608@gmail.com>
2016-04-19 12:57:29 -0400

    tried to re add backstretch jQuery function

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
                 </div>
             </section>
                     
- <script src="js/jquery-2.1.4.min.js"></script>
+ <script src="js/jquery-2.1.4.min (1).js"></script>   
  <script src="js/jquery.backstretch.min.js"></script>
  <script src="js/main.js"></script>
 
